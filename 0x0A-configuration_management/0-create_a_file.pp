33fe07c0614db521e828a3366dbb9c48f363609d
sarah-kamall <sarahkamal1712@gmail.com>
2024-04-25 02:31:42 +0200

    0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+file { '/tmp/school':
+  ensure  => present,   # Ensure the file exists
+  content => 'I love Puppet',   # Content of the file
+  mode    => '0744',    # Permissions for the file
+  owner   => 'www-data',    # Owner of the file
+  group   => 'www-data',    # Group of the file
+}
