765db6012f0ac691c3a469a09b9359b558a4b849
sarsora <sarah.kamal.soliman@gmail.com>
2024-06-19 01:08:24 +0300

    pep8

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,12 +9,12 @@
 import requests
 
 
-def count_words(subreddit, word_list, after=None, count = {}):
+def count_words(subreddit, word_list, after=None, count={}):
     """
         queries the Reddit API and
         returns the top 10 posts
     """
-  
+
     ENDPOINT = 'http://www.reddit.com/r/' + subreddit + '/hot.json'
     HEADERS = {
         "User-Agent": "Custom"
@@ -34,18 +34,19 @@ def count_words(subreddit, word_list, after=None, count = {}):
     else:
         posts = RESPONSE.json().get("data").get("children")
         for post in posts:
-            title= post.get("data").get('title')
+            title = post.get("data").get('title')
             for word in word_list:
                 word_comp = word.lower()
                 title_comp = title.lower()
                 if (word_comp in title_comp):
                     if count.get(word) is None:
-                        count[word]=0
-                    count[word]+=1
+                        count[word] = 0
+                    count[word] += 1
         after = RESPONSE.json().get("data").get("after")
         if(after is not None):
             count_words(subreddit, word_list, after, count)
         else:
-            sorted_dict = dict(sorted(count.items(), key=lambda item: item[1], reverse=True))
+            sorted_dict = dict(sorted(count.items(), key=lambda item: item[1],
+                                      reverse=True))
             for wordp, counterp in sorted_dict.items():
                 print(f"{wordp.lower()}: {counterp}")
