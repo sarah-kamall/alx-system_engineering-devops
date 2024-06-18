41c82cf49174b37298bf1813f9476f61ba39fb84
sarsora <sarah.kamal.soliman@gmail.com>
2024-06-19 00:11:22 +0300

    1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+    queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for
+    a given subreddit.
+"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+        queries the Reddit API and
+        returns the top 10 posts
+    """
+    ENDPOINT = 'http://www.reddit.com/r/' + subreddit + '/hot.json'
+    HEADERS = {
+        "User-Agent": "Custom"
+    }
+    PARAMS = {
+        "limit": 10
+    }
+    RESPONSE = requests.get(ENDPOINT, headers=HEADERS, params=PARAMS)
+    if RESPONSE.status_code != 200:
+        print(None)
+    else:
+        posts = RESPONSE.json().get("data").get("children")
+        for post in posts:
+            print(post.get("data").get('title'))
