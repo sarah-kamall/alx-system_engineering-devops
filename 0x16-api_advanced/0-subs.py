37062ebf6a7888e26ec65b2c1d7573c1f975f887
sarsora <sarah.kamal.soliman@gmail.com>
2024-06-18 23:38:20 +0300

    pip8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,12 +3,17 @@
 """
     function that queries the Reddit API and returns
     the number of subscribers (not active users, total subscribers)
-    for a given subreddit. If an invalid subreddit is given, the function should return 0
+    for a given subreddit. If an invalid subreddit is given,
+    the function should return 0
 """
+
+
 import requests
+
+
 def number_of_subscribers(subreddit):
     """
-        queries the Reddit API and 
+        queries the Reddit API and
         returns the number of subscribers
     """
     ENDPOINT = 'http://www.reddit.com/r/' + subreddit + '/about.json'
